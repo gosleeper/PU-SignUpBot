36b537686577b092d37d85d125d76e8806ef83f9
gosleeper <85383869+gosleeper@users.noreply.github.com>
2025-03-12 15:52:18 +0800

    Update headers.py
    改成linux的请求头

--- utils/headers.py
+++ utils/headers.py
@@ -1,5 +1,5 @@
 HEADERS_ACTIVITY_INFO = {
-        #     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
+             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
         #     "Accept": "application/json, text/plain, */*",
         #     "Accept-Language": "en-US,en;q=0.5",
         #     "Accept-Encoding": "gzip, deflate, br",
@@ -13,13 +13,13 @@ HEADERS_ACTIVITY_INFO = {
         #     "Sec-Fetch-Site": "same-site",
         #     "Pragma": "no-cache",
         #     "Cache-Control": "no-cache"
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36",
+        #"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36",
         "Content-Type": "application/json",
         "Accept": "application/json, text/plain, */*"
 }
 
 HEADERS_ACTIVITY = {
-        # "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
+         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
         # "Accept": "application/json, text/plain, */*", "Accept-Language": "en-US,en;q=0.5",
         # "Accept-Encoding": "gzip, deflate, br", "Content-Type": "application/json",
         # "Authorization": "",
@@ -27,13 +27,13 @@ HEADERS_ACTIVITY = {
         # "Referer": "https://class.pocketuni.net/", "Sec-Fetch-Dest": "empty",
         # "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-site", "Pragma": "no-cache",
         # "Cache-Control": "no-cache"
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36",
+        #"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36",
         "Content-Type": "application/json",
         "Accept": "application/json, text/plain, */*"
 }
 
 HEADERS_LOGIN = {
-        # "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
+         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
         # "Accept": "application/json, text/plain, */*",
         # "Accept-Language": "en-US,en;q=0.5",
         # "Accept-Encoding": "gzip, deflate, br",
@@ -46,7 +46,7 @@ HEADERS_LOGIN = {
         # "Sec-Fetch-Site": "same-site",
         # "Pragma": "no-cache",
         # "Cache-Control": "no-cache"
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36",
+        #"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36",
         "Content-Type": "application/json",
         "Accept": "application/json, text/plain, */*"
 }
@@ -56,7 +56,7 @@ HEADERS_GET_SCHOOL = {
         # 'sec-ch-ua-mobile':'?0',
         # 'sec-ch-ua-platform':'"Windows"',
         # 'Upgrade-Insecure-Requests':'1',
-        # 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
+         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
         # 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         # 'Sec-Fetch-Site':'same-origin',
         # 'Sec-Fetch-Mode':'navigate',
@@ -65,7 +65,7 @@ HEADERS_GET_SCHOOL = {
         # 'Referer':'https://pocketuni.net/',
         # 'Accept-Encoding':'gzip, deflate, br, zstd',
         # 'Accept-Language':'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36",
+        #"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36",
         "Content-Type": "application/json",
         "Accept": "application/json, text/plain, */*"
 }
