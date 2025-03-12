26a80970c179ba2b0dded688e8c3a2107c51ef22
gosleeper <85383869+gosleeper@users.noreply.github.com>
2025-03-12 15:45:58 +0800

    Update single.py

--- utils/single.py
+++ utils/single.py
@@ -3,17 +3,25 @@
 """
 import threading
 from utils.activity_bot import ActivityBot
+import queue
+
 
 def single_account(user_data:dict,file_path = "activity_ids.txt"):
     print('线程：'+user_data['userName'])
     pass
     bot = ActivityBot(user_data)
     try:
-        with open(file_path, 'r') as file:
-            activity_ids = [int(line.strip()) for line in file if line.strip()]
+        # with open(file_path, 'r') as file:
+        #     activity_ids = [int(line.strip()) for line in file if line.strip()]
+        from queue_manager import ACTIVITY_queue
+        print("zheli")
+        ACTIVITY = ACTIVITY_queue.get()
+        print("活动ID列表：",ACTIVITY)
+        activity_ids = ACTIVITY.copy()
         threads = []
         for activity_id in activity_ids:
             thread = threading.Thread(target=bot.signup, args=(activity_id,))
+            print(activity_id,"开始执行线程")
             threads.append(thread)
             thread.start()
 
@@ -21,4 +29,13 @@ def single_account(user_data:dict,file_path = "activity_ids.txt"):
             thread.join()
     except FileNotFoundError:
         print("线程："+user_data['userName']+"活动ID文件不存在")
-    
+
+# 设置一个默认用来更新的账号
+def update_activities(user_data ={
+        "userName": "202211070519",
+        "password": "@qwer123456",
+        "sid": 208754666766336,
+        "device": "pc"
+    }):
+    bot = ActivityBot(user_data)
+    bot.get_all_activity()
